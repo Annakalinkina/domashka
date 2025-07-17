5ba2ec8035a15da542fa5e48168f86760d947e01
Annakalinkina <148589765+Annakalinkina@users.noreply.github.com>
2025-07-17 20:22:06 +0300

    Delete 07_lesson directory

--- 07_lesson/cart_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class CartPage:
-    def __init__(self, browser):
-        self.browser = browser
-
-    def checkout(self):
-        self.browser.find_element(By.ID, "checkout").click()
